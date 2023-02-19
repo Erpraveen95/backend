a2a994ab9ef0ca0c70751496a0b5ddf95f0ccc5d
Urs-Praveen <work.erpraveen@gmail.com>
2023-02-19 19:26:25 +0530

    clean up code by creating a separate routes.js file and module.exports

---
+++ routes.js
@@ -0,0 +1,51 @@
+const fs = require('fs')
+const requestHandler = (req,res)=>{
+        const url = req.url
+        const method = req.method;
+        if(url === '/'){
+             fs.readFile("message.txt",{encoding:'utf-8'},(err,data)=>{
+                 console.log(err)
+                 res.write('<html>')
+                 res.write('<head><title>This is My webpage</title></head>')
+             
+                 res.write('<body><h1>Welcome Home<h1>')
+                 res.write(`<span>${data}<span>`)
+                 res.write('<form action="/message" method="POST"><input type="text" name = "message"><button type"submit">go to about</button></form></body>')
+                 res.write('</html>')
+                 return res.end()
+              //process.exit()
+         })
+        
+     } else if(url === '/message' && method==='POST'){
+             const body =[]
+             req.on('data',chunk =>{
+                 body.push(chunk)
+             })
+             return req.on('end',()=>{
+                 const parsedBody = Buffer.concat(body).toString()
+                 const message = parsedBody.split('=')[1]
+                 fs.writeFile('message.txt', message,err =>{
+                     console.log(err)
+                     res.statusCode = 302;
+                     res.setHeader('Location','/')
+                     return res.end()
+                 })
+             })
+         }
+         else{
+             res.setHeader('Content-Type','text/html')
+         res.write('<html>')
+         res.write('<head><title>This is My webpage</title></head>')
+         res.write('<body><h1>Welcome Home<h1>')
+         res.write('</html>')
+         res.end()
+         }
+     }
+//module.exports = requestHandler;
+/* 
+module.exports = {
+    handler : requestHandler,
+    text : "some text"
+} */
+exports.handler = requestHandler;
+exports.someText = "jai shree ram"
