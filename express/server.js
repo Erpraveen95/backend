2821762a1cc2cc80f1073ca79582c7121cc3357e
Urs-Praveen <112169678+Urs-Praveen@users.noreply.github.com>
2023-02-20 13:20:13 +0530

    added body parser

--- express/server.js
+++ express/server.js
@@ -1,12 +1,22 @@
 const express = require('express')
 
-const app = express();
+const bodyParser = require('body-parser');
 
-app.use((req,res,next)=>{
+const app = express();
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.use('/add-product',(req,res)=>{
     console.log('in the middleware')
-    next()
+    res.send('<html><body><form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form></body></html>')
+    res.send('<h1> Add product page!!</h1>')
+    
 })
-app.use((req,res,next)=>{
+app.get('/product',(req,res,next)=>{
+    console.log(req.body)
+    
+    res.redirect('/')
+})
+app.use('/',(req,res,next)=>{
     console.log('in another middleware')
     res.send('<h1> Welcome To Express!!</h1>')
 })
